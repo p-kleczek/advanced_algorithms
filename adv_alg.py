1540ab49430337d8c962af73f47ba511d2b9c960
Paweł Kłeczek <p.l.kleczek@gmail.com>
2014-10-20 23:25:55 +0200

    Sample for both graph versions

--- adv_alg.py
+++ adv_alg.py
@@ -2,8 +2,8 @@ from graphs.graph import MatrixGraph, Edge, ListGraph
 
 __author__ = 'pawel'
 
-# g = ListGraph()
-g = MatrixGraph()
+lg = ListGraph()
+mg = MatrixGraph()
 
 _id = 1
 with open('data/graf.txt') as f:
@@ -11,7 +11,13 @@ with open('data/graf.txt') as f:
         vals = line.strip().split('; ')
         e = Edge(_id, int(vals[0]), int(vals[1]), int(vals[2]))
         _id += 1
-        g.add_edge(e)
+        lg.add_edge(e)
+        mg.add_edge(e)
 
-print g
-print "\nControl sum = %s " % (sum ([e.weight for e in g._get_edges() if e.vertex_from < e.vertex_to]), )
+print 'MATRIX graph'
+print mg
+print "\nControl sum = %s " % (sum ([e.weight for e in mg._get_edges() if e.vertex_from < e.vertex_to]), )
+print
+print 'LIST graph'
+print lg
+print "\nControl sum = %s " % (sum ([e.weight for e in lg._get_edges() if e.vertex_from < e.vertex_to]), )
